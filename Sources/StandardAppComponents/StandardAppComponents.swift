7f6d789b0a94bf98a351cd77b10bf95b4aa97c3a
jiikko <n905i.1214@gmail.com>
2026-05-01 09:55:09 +0900

    Trim README and module header to minimum (private use)

--- Sources/StandardAppComponents/StandardAppComponents.swift
+++ Sources/StandardAppComponents/StandardAppComponents.swift
@@ -1,14 +1,3 @@
-// StandardAppComponents
-//
-// jiikko の macOS アプリ群が共通で使う UI 規約と振る舞いを提供する SPM パッケージ。
-//
-// 現在は最小スケルトン。実装は段階的に追加していく:
-// - Settings/      … SettingsWindow / GeneralTabContract
-// - MenuBar/       … MenuBarAgent / MenuBarContract / MenuBarVisibilitySection
-// - Lifecycle/     … LaunchAtLoginContract / AboutContract
-// - Behaviors/     … standardSettingsBehaviors / autoSaveWindowFrame 等
-// - Internal/      … NotImplementedSlot 等
-
 import Foundation
 
 public enum StandardAppComponents {
